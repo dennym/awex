355242cb0008287ca151e594ec3e9365eb50e8ed
Denny Mueller <denny@dennymueller.de>
2023-04-01 01:38:49 +0200

    initial commit

---
+++ lib/awex.ex
@@ -0,0 +1,18 @@
+defmodule Awex do
+  @moduledoc """
+  Documentation for `Awex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Awex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/awex_test.exs
@@ -0,0 +1,8 @@
+defmodule AwexTest do
+  use ExUnit.Case
+  doctest Awex
+
+  test "greets the world" do
+    assert Awex.hello() == :world
+  end
+end
