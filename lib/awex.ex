52bcb5e95e399fa5db96441f58e3d87d2a4d5215
Denny Mueller <denny@dennymueller.de>
2023-04-03 17:11:51 +0200

    WIP

--- lib/awex.ex
+++ lib/awex.ex
@@ -1,18 +1,28 @@
 defmodule Awex do
   @moduledoc """
-  Documentation for `Awex`.
-  """
+  Base Module for the API Client.
 
-  @doc """
-  Hello world.
+  The API Documentation for awx can be found under https://docs.ansible.com/ansible-tower/latest/html/towerapi/api_ref.html.
 
-  ## Examples
+  ## Requirements:
 
-      iex> Awex.hello()
-      :world
+    * Tesla: the api client depends on Tesla. Every API call needs an instance of a `Tesla.Client` to be able to do the rest calls.any()
 
   """
-  def hello do
-    :world
+
+  alias Awex.Client
+
+  @type response ::
+          {:ok, term, HTTPoison.Response.t()}
+          | {integer, any, HTTPoison.Response.t()}
+          | pagination_response
+
+  @spec get(binary, Client.t()) :: response
+  def get(path, client, params \\ [], options \\ []) do
+
+  end
+
+  def put(path, client, params \\ [], options \\ []) do
+
   end
 end
