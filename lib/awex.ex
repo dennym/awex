23c9c790a4a29fd6e9b0f01073d89396788a1691
Denny Mueller <denny@dennymueller.de>
2023-04-07 18:13:43 +0200

    add some example req

--- lib/awex.ex
+++ lib/awex.ex
@@ -1,4 +1,8 @@
 defmodule Awex do
+  use HTTPoison.Base
+  alias Awex.Client
+  alias Jason
+
   @moduledoc """
   Base Module for the API Client.
 
@@ -10,19 +14,27 @@ defmodule Awex do
 
   """
 
-  alias Awex.Client
-
   @type response ::
           {:ok, term, HTTPoison.Response.t()}
           | {integer, any, HTTPoison.Response.t()}
           | pagination_response
 
+  @type pagination_response :: {response, binary | nil, Client.auth()}
+
   @spec get(binary, Client.t()) :: response
   def get(path, client, params \\ [], options \\ []) do
-
+    url =
+      client
+      |> url(path)
+      |> add_params_to_url(params)
   end
 
   def put(path, client, params \\ [], options \\ []) do
 
+  end
+
+  @spec url(client :: Client.t(), path :: binary) :: binary
+  defp url(_client = %Client{endpoint: endpoint}, path) do
+    endpoint <> path
   end
 end
--- lib/awex/jobs.ex
+++ lib/awex/jobs.ex
@@ -1,2 +1,32 @@
 defmodule Awex.Jobs do
+  import Awex
+
+  alias Awex.Client
+
+  @doc """
+  Get a single `job`.
+
+  ## Example
+
+      Awex.Jobs.find client, "edgurgel"
+      Awex.Jobs.find client, "iurifq"
+
+  More info at: https://docs.ansible.com/ansible-tower/latest/html/towerapi/api_ref.html#/Jobs/Jobs_jobs_read_0
+  """
+  @spec find(Client.t(), binary) :: Awex.response()
+  def find(client \\ %Client{}, job) do
+    get("jobs/#{job}", client)
+  end
+
+  @doc """
+  Get all users.
+  ## Example
+      Tentacat.Users.list
+      Tentacat.Users.list client
+  More info at: http://developer.github.com/v3/users/#get-all-users
+  """
+  @spec list(Client.t(), any) :: Tentacat.response()
+  def list(client \\ %Client{}, options \\ []) do
+    get("jobs", client, [], Keyword.merge([pagination: :none], options))
+  end
 end
